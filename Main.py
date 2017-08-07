3f4890a9a965f9cb0499ac1e00cf8c53dc70b835
Xbolanos <xbolanosfonseca@gmail.com>
2017-08-07 12:16:07 -0600

    Modularizado
    Aunque sigue desordenado

--- Main.py
+++ Main.py
@@ -2,9 +2,5 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 #ABRIR IMAGENES 
-print cv2.__version__
-img = cv2.imread('1.jpg',0)
-plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.show()
+import managerImages
 
