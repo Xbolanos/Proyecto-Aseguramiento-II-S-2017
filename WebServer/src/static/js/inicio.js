3cb00840b0baae07d70e186c9cfce6ff44040e65
Nicol Morice <nicol.morice@gmail.com>
2017-08-19 07:05:09 -0600

    JS documentado

--- WebServer/src/static/js/inicio.js
+++ WebServer/src/static/js/inicio.js
@@ -1,27 +1,47 @@
-window.onload = function() {
+
+
+window.onload = function() {        
+		/*
+        @summary: This function will run once the entire page (including images or frames),
+        not just the DOM(Document Object Model), is ready. 
+        
+        Parameters
+        ----------
+        @param self: part of OOP syntax 
+        fileInput: gets the input with id:fileInput in the html file.
+        fileDisplayArea: gets the div with id:fileDisplayArea
+        file: gets the chosen image by the user from the input fileInput.
+        imageType: define the type of the content we are waiting
+        reader: Returns a newly constructed FileReade.
+        img: creates a new HTMLImageElement instance.
+        
+        Returns
+        ----------
+        @return: Displays the image selected by the user in the page
+        */
 
 		var fileInput = document.getElementById('fileInput');
 		var fileDisplayArea = document.getElementById('fileDisplayArea');
+	
 
-
-		fileInput.addEventListener('change', function(e) {
+		fileInput.addEventListener('change', function(e) { //When the user selects another image
 			var file = fileInput.files[0];
 			var imageType = /image.*/;
 
-			if (file.type.match(imageType)) {
+			if (file.type.match(imageType)) { //Verify the content is a image, not other type
 				var reader = new FileReader();
 
-				reader.onload = function(e) {
-					fileDisplayArea.innerHTML = "";
+				reader.onload = function(e) { //After the page has been loaded
+					fileDisplayArea.innerHTML = ""; //Change the HTML content of  fileDisplayArea
 
 					var img = new Image();
-					img.src = reader.result;
+					img.src = reader.result; //.src =Change the URL of an image  .result =Returns the file's contents
 
-					fileDisplayArea.appendChild(img);
+					fileDisplayArea.appendChild(img); // Append an image in fileDisplayArea
 				}
 
-				reader.readAsDataURL(file);	
-			} else {
+				reader.readAsDataURL(file);	 // read the contents of the file
+			} else { //User selected another type of file
 				fileDisplayArea.innerHTML = "File not supported!"
 			}
 		});
