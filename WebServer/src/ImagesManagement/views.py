18dd9c9ffa814e4a1df3809cd4e58f6c33cd9ef1
Xbolanos <xbolanosfonseca@gmail.com>
2017-08-20 08:33:36 -0600

    Documentación interna, Django

--- WebServer/src/ImagesManagement/views.py
+++ WebServer/src/ImagesManagement/views.py
@@ -3,26 +3,63 @@ from django import forms
 from django.core.files.storage import FileSystemStorage
 
 class UploadFileForm(forms.Form):
+       """
+        @summary: This function is in charge of upload files form.
+        
+        Parameters
+        ----------
+        @param form.Form= is the form that going to be upload 
+        
+        Returns
+        ----------
+        @return: void
+        """
     title = forms.CharField(max_length=100)
     file = forms.ImageField()
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "holii"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+   """
+        @summary: This function is in charge of get the html.
+        
+        Parameters
+        ----------
+        @param request: is the state from the system 
+        
+        Returns
+        ----------
+        @return: HttpResponse
+        """
     return render(request, 'html/inicio.html', context_dict)
 
 def upload_image(request):
+     """
+        @summary: This function is in charge of get the html.
+        
+        Parameters
+        ----------
+        @param request: is the state from the system 
+        
+        Returns
+        ----------
+        @return: HttpResponse
+        """
     if request.method == 'POST':
         proccess_files(request.FILES.getlist('files'))
         
     return render(request, 'html/inicio.html')
 
 def proccess_files(files):
+     """
+        @summary: This function is in charge of get all files that was sending in the front end.
+        
+        Parameters
+        ----------
+        @param files: array of files 
+        
+        Returns
+        ----------
+        @return: void
+        """
     fs = FileSystemStorage()
     
     for file in files:
