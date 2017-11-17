e0b2ebaa52534efc9f63a08b81d5166b52a3d0ad
Xbolanos <xbolanosfonseca@gmail.com>
2017-11-17 17:49:24 -0600

    Arreglitos

--- WebServer/src/controller/system.py
+++ WebServer/src/controller/system.py
@@ -26,6 +26,7 @@ from WebServer.settings import MEDIA_ROOT
 
 im = ImagesManager()
 
+
 def get_index_page():
     """
     @summary: a basic function to retrieve the home page of the website and
@@ -61,20 +62,18 @@ def train_system(files, filesData, autovectors, images_per_subject):
     basePath = fs.location
     paths = []
     subjects_names = []
-    
     for key in files.keys():
         file = files[key]
         fileData = filesData[key + 'data']
-        
         if fileData not in subjects_names:
             subjects_names.append(fileData)
-        
+
         fs.location = basePath + '/' + fileData
-        
+
         fileName = fs.save(file.name, file)
         fileUrl = fs.location + '/' + fileName
         paths.append(fileUrl)
-    
+
     im.add_images_paths(paths, subjects_names)
     im.load_images()
 
@@ -83,9 +82,9 @@ def train_system(files, filesData, autovectors, images_per_subject):
 
     return {'type': 'success',
             'title': '¡Registrado!',
-            'message': 'Se ha(n) registrado con exito al sistema.'
-    }
-    
+            'message': 'Se ha(n) registrado con exito al sistema.'}
+
+
 def recognize_subject(handler):
     path = STATICFILES_DIRS[0]  # The static path for de subjects images.
 
@@ -94,26 +93,25 @@ def recognize_subject(handler):
     extension = filelink.get_metadata()['filename'][-4:]
     fullpath = path + '/subjects/' + handler + extension
     filelink.download(fullpath)
-    
+
     tempIM = ImagesManager()
     tempIM.add_images_paths([fullpath])
     tempIM.load_images()
-    
+
     rm = Recognize()
     index = rm.process(tempIM, mode=1)
     result = im.get_subject_name(index - 1)
-    
     return {'type': 'success',
             'title': 'Se ha reconocido al sujeto',
-            'message': 'El rostro pertenece al sujeto: ' + str(result)
-    }
-    
+            'message': 'El rostro pertenece al sujeto: ' + str(result)}
+
+
 def signin(user):
     admin = {
         'email': 'admin@reconoceme.com',
         'password': '123Queso'
     }
-    
+
     if(user['email'] == admin['email']):
         if(user['password'] == user['password']):
             return True
